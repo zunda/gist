a87fc33764a98049ab91e9ae3788426b4f6fdcbc
zunda <zunda at freeshell.org>
2009-09-03 15:32:18 -1000

    Added -a and -p options to make gist anoynmous and private

--- gist-cat.rb
+++ gist-cat.rb
@@ -33,9 +33,10 @@ class Gist
 
 		attr_reader :url
 
-		def initialize(private = false)
+		def initialize(private = false, anonymous = false)
 			@contents = Array.new
 			@private = private
+			@anonymous = anonymous
 		end
 
 		def add(data, path = nil)
@@ -52,11 +53,11 @@ class Gist
 				h["file_contents[gistfile#{idx+1}]"] = data
 			end
 			h['private'] = 'on' if @private
-			h.merge(Push.auth)
+			h.merge!(Push.auth) unless @anonymous
 			return h
 		end
 
-		def post
+		def post!
 			res = Net::HTTP::Proxy(Gist.proxy).post_form(Push.url, form)
 			@url = res['location']
 			return self
@@ -65,17 +66,37 @@ class Gist
 end
 
 if __FILE__ == $0
+	require 'optparse'
+	opt = OptionParser.new
+	opt.banner = "Usage: #{File.basename($0)} [options] [file(s)]\nPosts stdin or file(s) to gist.github.com"
+
+	is_anon = false
+	is_priv = false
+	opt.on('-a', '--anonymous', 'Posts gist as anonymous'){is_anon = true}
+	opt.on('-p', '--private', 'Makes gist private'){is_priv = true}
+	opt.on('-h', '--help', 'Prints this message and quit'){
+		puts opt.help
+		exit 0
+	}
 	begin
-		gist = Gist::Push.new
+		opt.parse!
+	rescue OptionParser::InvalidOption
+		$stderr.puts $!
+		$stderr.puts opt.help
+		exit 1
+	end
+
+	begin
+		gist = Gist::Push.new(is_priv, is_anon)
 		ARGV.each do |src|
 			File.open(src) do |f|
 				gist.add(f.read, src)
 			end
 		end
-		gist.post
+		gist.post!
 		puts gist.url
 	rescue Gist::Error
-		$stderr.puts "#{$0}: #{$!}"
+		$stderr.puts $!
 		exit 1
 	end
 end
